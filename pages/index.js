d443f47299bdd08b5ab0e9bc0f9f13a95e36af25
Caglar Turali <caglarturali@gmail.com>
2019-11-16 19:56:55 +0300

    Properly setup the app for MUI SSR

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,14 @@
-import React, { Fragment } from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import React from 'react';
 import MainLayout from '../src/layouts/MainLayout';
 import KernelList from '../src/components/KernelList';
 import { KernelsProvider } from '../src/contexts';
 
 const Home = () => (
-  <Fragment>
-    <CssBaseline />
-    <MainLayout>
-      <KernelsProvider>
-        <KernelList />
-      </KernelsProvider>
-    </MainLayout>
-  </Fragment>
+  <MainLayout>
+    <KernelsProvider>
+      <KernelList />
+    </KernelsProvider>
+  </MainLayout>
 );
 
 export default Home;
--- src/components/DocumentHead/index.js
+++ src/components/DocumentHead/index.js
@@ -9,11 +9,6 @@ const DocumentHead = ({ pageTitle }) => {
   return (
     <Head>
       <title>{pageTitle && `${pageTitle} | `} GetKernel.sh</title>
-      <link rel="icon" href="/favicon.ico" />
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-      />
     </Head>
   );
 };
