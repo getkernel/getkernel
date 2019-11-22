a602cddb3d5b9bce3a7278657dfc5781886598e8
Caglar Turali <caglarturali@gmail.com>
2019-11-22 21:58:45 +0300

    Fix Babel config

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,4 @@
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
-        },
-      },
-    ],
-    '@babel/preset-react',
-    'next/babel',
-  ],
-  plugins: [
-    '@babel/plugin-proposal-do-expressions',
-    '@babel/plugin-proposal-class-properties',
-  ],
+  presets: ['next/babel'],
+  plugins: [],
 };
