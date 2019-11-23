5ea2b36f549d26edb85651dd40098915b7d6f5a2
Caglar Turali <caglarturali@gmail.com>
2019-11-23 22:23:18 +0300

    Implement Version class

---
+++ src/models/Version.js
@@ -0,0 +1,69 @@
+/**
+ * Version class that implements static parse method and
+ * overridden toString method.
+ */
+export default class Version {
+  /**
+   * Creates a Version object from a Version string
+   * @param {String} versionString
+   */
+  constructor(versionString) {
+    const versionObj = this.constructor.parse(versionString);
+
+    // Attach properties to the instance.
+    Object.keys(versionObj).forEach((key) => {
+      this[`_${key}`] = versionObj[key];
+    });
+  }
+
+  /**
+   * Parses version string into an object.
+   * @param {String} versionString Version string
+   */
+  static parse(versionString) {
+    let major, minor, patch, rc, distro;
+    try {
+      const regex = /^v?(\d+)\.(\d+)(?:\.(\d+))?(?:-(rc\d+))?(?:-(\w+))?/i;
+      [, major, minor, patch, rc, distro] = versionString.match(regex);
+    } catch (_) {}
+
+    return { major, minor, patch, rc, distro };
+  }
+
+  get major() {
+    return this._major;
+  }
+  set major(_) {}
+
+  get minor() {
+    return this._minor;
+  }
+  set minor(_) {}
+
+  get patch() {
+    return this._patch;
+  }
+  set patch(_) {}
+
+  get rc() {
+    return this._rc;
+  }
+  set rc(_) {}
+
+  get distro() {
+    return this._distro;
+  }
+  set distro(_) {}
+
+  /**
+   * Returns string representation of the Version instance.
+   */
+  toString() {
+    const { major, minor, patch, rc, distro } = this;
+    let vString = `v${major}.${minor}`;
+    if (patch) vString += `.${patch}`;
+    if (rc) vString += `-${rc}`;
+    if (distro) vString += `-${distro}`;
+    return vString;
+  }
+}
