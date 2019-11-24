023215333efaeee7b289dd3335566c5a0560642c
Caglar Turali <caglarturali@gmail.com>
2019-11-24 18:02:20 +0300

    Remove undefined import/exports

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,8 @@
 import buildChecksums from './buildChecksums';
 import buildVariants from './buildVariants';
 import { fileDownload, batchDownload, calculateDownloadSize } from './download';
-import { encodeBookmarks, decodeBookmarks } from './bookmarks';
 
 export {
-  encodeBookmarks,
-  decodeBookmarks,
   buildChecksums,
   buildVariants,
   fileDownload,
