6a934bc7d6dfc8d717be3141e57f632d7a55cc91
Caglar Turali <caglarturali@gmail.com>
2019-11-24 21:17:33 +0300

    Add tooltips

--- src/components/KernelListItem/index.js
+++ src/components/KernelListItem/index.js
@@ -3,6 +3,7 @@
  */
 import React, { useContext } from 'react';
 import Link from 'next/link';
+import moment from 'moment';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
@@ -12,7 +13,7 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Chip from '@material-ui/core/Chip';
 import Typography from '@material-ui/core/Typography';
-import moment from 'moment';
+import Tooltip from '@material-ui/core/Tooltip';
 import IconButton from '@material-ui/core/IconButton';
 import BookmarkIcon from '@material-ui/icons/Bookmark';
 import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
@@ -85,13 +86,18 @@ const KernelListItem = ({ version_name, version_slug, last_modified }) => {
           </CardActionArea>
         </Link>
         <CardActions disableSpacing className={classes.actions}>
-          <IconButton
-            size="small"
-            onClick={handleBookmarkClick}
-            aria-label="add to bookmarks"
+          <Tooltip
+            title={isBookmarked ? 'Remove from bookmarks' : 'Add to bookmarks'}
+            aria-label="toggle bookmark"
           >
-            {isBookmarked ? <BookmarkIcon /> : <BookmarkBorderIcon />}
-          </IconButton>
+            <IconButton
+              size="small"
+              onClick={handleBookmarkClick}
+              aria-label="toggle bookmark"
+            >
+              {isBookmarked ? <BookmarkIcon /> : <BookmarkBorderIcon />}
+            </IconButton>
+          </Tooltip>
         </CardActions>
       </Card>
     </Grid>
