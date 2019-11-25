5d84005ec31704dfa6894b43906c5ee83a7fbf90
Caglar Turali <caglarturali@gmail.com>
2019-11-25 21:36:07 +0300

    Add slogan - update DocumentHead comp

--- src/app.config.js
+++ src/app.config.js
@@ -1,6 +1,7 @@
 export default {
-  name: 'getkernel.sh',
   baseUrl: 'https://getkernel.sh',
+  name: 'getkernel.sh',
+  slogan: 'The Kernel Store for Debian/Ubuntu Derivatives',
   smoothScrollDuration: 500,
   downloadInterval: 1500,
 };
--- src/components/DocumentHead/index.js
+++ src/components/DocumentHead/index.js
@@ -4,11 +4,15 @@
 import React from 'react';
 import Head from 'next/head';
 import PropTypes from 'prop-types';
+import appConfig from '../../app.config';
 
 const DocumentHead = ({ pageTitle }) => {
+  const staticText = `${appConfig.slogan} | ${appConfig.name}`;
+  const title = pageTitle ? `${pageTitle} | ${staticText}` : staticText;
+
   return (
     <Head>
-      <title>{pageTitle && `${pageTitle} | `} GetKernel.sh</title>
+      <title>{title}</title>
     </Head>
   );
 };
