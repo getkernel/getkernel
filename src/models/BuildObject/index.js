5a474068da55aac9f417bc0d4398bf614e1ba92f
Caglar Turali <caglarturali@gmail.com>
2019-12-01 18:56:46 +0300

    Implement variants getter

--- src/models/BuildObject/index.js
+++ src/models/BuildObject/index.js
@@ -1,4 +1,6 @@
 import './typedef';
+import Compare from '../../utils/Compare';
+import { BUILD_VARIANT_ALL } from '../../constants';
 
 /**
  * Class for storing build objects by platform.
@@ -24,5 +26,37 @@ export default class BuildObject {
    */
   static parse({ platform, buildStatus, binaries }) {
     return new this(platform, buildStatus, binaries);
+  }
+
+  /**
+   * Returns build variants. (e.g. generic, lowlatency)
+   */
+  get variants() {
+    const base = this.binaries.find(({ fileName }) =>
+      fileName.endsWith('_all.deb'),
+    );
+    const rest = this.binaries.filter(
+      ({ fileName }) => fileName !== base.fileName,
+    );
+
+    const variants = new Set();
+
+    const tokenBase = base.fileName
+      .replace('linux-headers-', '')
+      .replace('_all.deb', '');
+
+    const [tokenStart, tokenFinish] = tokenBase.split('_');
+
+    rest.forEach(({ fileName }) => {
+      const variant = fileName.substring(
+        fileName.indexOf(tokenStart) + tokenStart.length + 1,
+        fileName.indexOf(tokenFinish) - 1,
+      );
+      variants.add(variant);
+    });
+
+    const variantsArray = [...variants].sort(Compare.string());
+
+    return [...variantsArray, BUILD_VARIANT_ALL];
   }
 }
