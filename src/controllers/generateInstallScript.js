ada96acfb53767b2464bf5f82df0dee4c0da05ca
Caglar Turali <caglarturali@gmail.com>
2019-12-03 17:41:27 +0300

    Improve install script

--- src/controllers/generateInstallScript.js
+++ src/controllers/generateInstallScript.js
@@ -1,5 +1,60 @@
 const fetch = require('isomorphic-unfetch');
 
+const buildScript = (binaries, version, kernelUrl, origin, platform) => {
+  let script = '';
+  const addLine = (line, extraLine = 0) => {
+    script += `${line || ''}\n`;
+    if (extraLine > 0) script += '\n'.repeat(extraLine);
+  };
+  const addComment = (comment) => {
+    script += `# ${comment}\n`;
+  };
+  const echo = (text, extraLine = 0) => {
+    addLine(`echo "\\e[33m${text}\\e[0m"`, extraLine);
+  };
+
+  const tempDir = '_TMP_DIR';
+  const binariesFiles = binaries.map(({ fileName }) => fileName);
+  const binariesUrls = binaries.map(
+    ({ fileName }) => `${kernelUrl}/${fileName}`,
+  );
+
+  addLine('#!/bin/bash');
+  addLine('set -ue', 1);
+
+  addComment('Set wget progress option.');
+  addLine(
+    "wget --help | grep -q '\\--show-progress' && _PROGRESS_OPT=\"-q --show-progress --progress=bar\" || _PROGRESS_OPT=''",
+    1,
+  );
+
+  addComment('Create a temp directory and run there.');
+  addLine(`${tempDir}=$(mktemp -d) && cd $${tempDir}`, 1);
+
+  echo(`This script will install Linux ${version} on your system.`, 1);
+
+  echo('Downloading binaries...');
+  binariesUrls.forEach((url) => {
+    addLine(`wget $_PROGRESS_OPT ${url}`);
+  });
+  addLine();
+
+  echo('Installing...');
+  addLine(`sudo dpkg -i ${binariesFiles.join(' ')}`, 1);
+
+  addComment('Delete temporary directory.');
+  echo('Cleaning up...');
+  addLine(`sudo rm -rf $${tempDir}`, 1);
+
+  echo('Installation complete!');
+  echo(
+    "It's recommended to restart your computer immediately for the changes to take effect.",
+  );
+  echo(`${origin}/kernel/${version}#${platform}`);
+
+  return script;
+};
+
 const generateInstallScript = async (origin, versionStr, platform, variant) => {
   const response = await fetch(`${origin}/api/kernel/${versionStr}`);
   const json = await response.json();
@@ -14,8 +69,7 @@ const generateInstallScript = async (origin, versionStr, platform, variant) => {
     return 'No results';
   }
 
-  const { version, kernelUrl, urls, builds } = results[0];
-  console.log(version, kernelUrl, urls);
+  const { version, kernelUrl, builds } = results[0];
 
   const build = builds.find((b) => b.platform === platform);
   const { buildStatus, binaries } = build;
@@ -27,45 +81,7 @@ const generateInstallScript = async (origin, versionStr, platform, variant) => {
     ({ fileName }) => fileName.includes(variant) || fileName.includes('_all'),
   );
 
-  let script = '';
-  const addLine = (line) => {
-    script += `${line || ''}\n`;
-  };
-  const addComment = (comment) => {
-    script += `# ${comment}\n`;
-  };
-
-  addLine('#!/bin/bash');
-  addLine('set -ue');
-  addLine();
-
-  addComment('Shows colorful output messages.');
-  addLine(`show_msg () { 
-    echo -e "\\e[33m$1\\e[0m"
-}`);
-  addLine();
-
-  addComment('Set wget progress option.');
-  addLine(
-    "wget --help | grep -q '\\--show-progress' && _PROGRESS_OPT='-q --show-progress --progress=bar' || _PROGRESS_OPT=''",
-  );
-  addLine();
-
-  addComment('Create a temp directory and run there.');
-  addLine('_TMP_DIR=$(mktemp -d) && cd $_TMP_DIR');
-  addLine();
-
-  addLine(
-    `show_msg "This script will install Linux ${version} on your system."`,
-  );
-  addLine();
-
-  addLine('show_msg "Downloading binaries..."');
-  selectedBinaries.forEach(({ fileName }) => {
-    addLine(`wget $_PROGRESS_OPT ${kernelUrl}/${fileName}`);
-  });
-
-  return script;
+  return buildScript(selectedBinaries, version, kernelUrl, origin, platform);
 };
 
 const controller = async (req, res) => {
