bc5161d86aa3e8a07f6254aef685890f298f516f
Caglar Turali <caglarturali@gmail.com>
2019-12-10 14:08:19 +0300

    Rename remaining tip's as tag

--- src/api/getKernel.js
+++ src/api/getKernel.js
@@ -4,12 +4,12 @@ import { ORIGIN } from '../constants';
 /**
  * Fetches kernel data for the specified version from API.
  * @param {String} versionStr Version string
- * @param {String} tip The tip that the version belongs to (optional).
+ * @param {String} tag The tag that the version belongs to (optional).
  * @returns {ApiResponse} ApiResponse object
  */
-const getKernel = async (versionStr, tip = null) => {
+const getKernel = async (versionStr, tag = null) => {
   let url = `${ORIGIN}/api/kernel/${versionStr}`;
-  if (tip) url += `?tip=${tip}`;
+  if (tag) url += `?tag=${tag}`;
   const res = await fetch(url);
   const json = await res.json();
   return json;
