859370993248e9fed206966bd02576a275e5644a
Caglar Turali <caglarturali@gmail.com>
2020-02-21 22:53:40 +0300

    Fix Now config

--- src/app.config.js
+++ src/app.config.js
@@ -1,8 +1,6 @@
-import nowConfig from '../now.json';
-
 export default {
-  origin: `https://${nowConfig.name}.now.sh`,
-  name: nowConfig.name,
+  origin: `https://getkernel.now.sh`,
+  name: 'getkernel',
   slogan: 'The Kernel Store for Ubuntu Derivatives',
   smoothScrollDuration: 500,
   downloadInterval: 1500,
